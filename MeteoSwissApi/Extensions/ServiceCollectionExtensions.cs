dbbbaf225c56465b8719337fc3db76772ade242f
Thomas Galliker <thomas@bluewin.ch>
2023-08-29 18:48:15 +0200

    Add DI extension methods

---
+++ MeteoSwissApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using MeteoSwissApi;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMeteoSwissApi(this IServiceCollection serviceCollection)
+        {
+            // Configuration
+            // TODO
+
+            // Register services
+            serviceCollection.AddSingleton<IMeteoSwissWeatherServiceOptions, MeteoSwissWeatherServiceOptions>();
+            serviceCollection.AddSingleton<IMeteoSwissWeatherService, MeteoSwissWeatherService>();
+
+            serviceCollection.AddSingleton<ISwissMetNetServiceOptions, SwissMetNetServiceOptions>();
+            serviceCollection.AddSingleton<ISwissMetNetService, SwissMetNetService>();
+
+            serviceCollection.AddSingleton<ISlfDataServiceOptions, SlfDataServiceOptions>();
+            serviceCollection.AddSingleton<ISlfDataService, SlfDataService>();
+
+            return serviceCollection;
+        }
+    }
+}
