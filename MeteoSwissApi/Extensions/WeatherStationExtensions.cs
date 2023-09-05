c11377ba3bddc06cc7dbc2bbbc2989049704a014
Thomas Galliker <thomas@bluewin.ch>
2023-09-05 16:58:40 +0200

    Update comment

--- MeteoSwissApi/Extensions/WeatherStationExtensions.cs
+++ MeteoSwissApi/Extensions/WeatherStationExtensions.cs
@@ -8,8 +8,12 @@ namespace MeteoSwissApi.Extensions
     public static class WeatherStationExtensions
     {
         /// <summary>
-        /// Gets weather stations which are nearby <paramref name="location"/> with a maxiumum radius of <paramref name="maxRadius"/>.
+        /// Gets weather stations which are nearby <paramref name="location"/> 
+        /// with a maxiumum radius of <paramref name="maxRadius"/>.
         /// </summary>
+        /// <returns>
+        /// List of <see cref="WeatherStation"/> with the respective distance to the given <paramref name="location"/>.
+        /// </returns>
         public static IEnumerable<(WeatherStation WeatherStation, Length Distance)> Nearby(this IEnumerable<WeatherStation> weatherStations, GeoCoordinate location, Length maxRadius)
         {
             var weatherStationsWithDistance = weatherStations
